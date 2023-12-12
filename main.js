5826ba123f5d163d98844026b8f37ddb93d05842
pramishka22 <143793055+pramishka22@users.noreply.github.com>
2023-12-12 08:34:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const bookingForm = document.getElementById("bookingForm");
 const bookBtn = document.getElementById("bookBtn");
-const resetCurrentBookingBtn = document.getElementById("currentBookingResetBtn");
+const resetCurrentBookingBtn = document.getElementById(
+  "currentBookingResetBtn"
+);
 const checkLoyaltyBtn = document.getElementById("checkLoyaltyBtn");
 const addToFavBtn = document.getElementById("addToFavoriteBtn");
 
@@ -28,77 +30,103 @@ let currentBookingPrice = 0;
 let overallBookingPrice = 0;
 
 // Extra Requirements
+function extraRequirements(){
+  this.name = "";
 if (wifi.checked) {
-	extraRequirements.push(" WiFi");
-} else if (wifi.checked = false) {
-	extraRequirements.pop(" WiFi");
+  extraRequirements.push(" WiFi");
+} else if ((wifi.checked = false)) {
+  extraRequirements.pop(" WiFi");
 }
 
 if (poolView.checked) {
-	extraRequirements.push(" Pool View");
-} else if (poolView.checked = false) {
-	extraRequirements.pop(" Pool View");
+  extraRequirements.push(" Pool View");
+} else if ((poolView.checked = false)) {
+  extraRequirements.pop(" Pool View");
 }
 
 if (gardenView.checked) {
-	extraRequirements.push(" Garden View");
-} else if (gardenView.checked = false) {
-	extraRequirements.pop(" Garden View");
+  extraRequirements.push(" Garden View");
+} else if ((gardenView.checked = false)) {
+  extraRequirements.pop(" Garden View");
 }
-
-
-// Calculate The Booking Price
-const singleRoomValue = 25000;
-const doubleRoomValue = 35000;
-const tripleRoomValue = 40000;
-if (roomType.value == "Single") {
-	roomValue = singleRoomValue;
-} else if (roomType.value == "Double") {
-	roomValue = doubleRoomValue;
-} else if (roomType.value == "Triple") {
-	roomValue = tripleRoomValue;
 }
-currentBookingPrice = roomValue*numberOfRooms.value*timeDuration.value;
-overallBookingPrice += currentBookingPrice;
+function calculateBooking() {
+  // Calculate The Booking Price
+  const singleRoomValue = 25000;
+  const doubleRoomValue = 35000;
+  const tripleRoomValue = 40000;
 
-if (promoCode.value == "Promo123") {
-	currentBookingPrice = 0.95*currentBookingPrice;
-} else {
-	currentBookingPrice = currentBookingPrice;
+  if (roomType.value == "Single") {
+    roomValue = singleRoomValue;
+  } else if (roomType.value == "Double") {
+    roomValue = doubleRoomValue;
+  } else if (roomType.value == "Triple") {
+    roomValue = tripleRoomValue;
+  }
+  currentBookingPrice = roomValue * numberOfRooms.value * timeDuration.value;
+  overallBookingPrice += currentBookingPrice;
+
+  if (promoCode.value == "Promo123") {
+    currentBookingPrice = 0.05 * currentBookingPrice;
+  } else {
+    currentBookingPrice = currentBookingPrice;
+  }
 }
-
 
 bookingForm.addEventListener("submit", function (event) {
-	event.preventDefault();
+  event.preventDefault();
 });
 
-bookBtn.addEventListener("click", function () {
-	bookingDetailsDiv.innerHTML = "<p>Room Type: " + roomType.value + "</p><p>Number of Rooms: " + numberOfRooms.value + "</p><p>Check In Date: " + checkIn.value + "</p><p>Check Out Date: " + checkOut.value + "</p><p>Number of Adults: " + numberOfAdults.value + "</p><p>Number of Children: " + numberOfChildren.value + "</p><p>Time Duration: " + timeDuration.value + " Days</p><p>Extra Requirements:" + extraRequirements + "</p><br>";
-	currentBookingPriceDiv.innerHTML = "<p> " + currentBookingPrice + "</p>";
-	overallBookingDiv.innerHTML = localStorage.getItem("overallBookingDiv");
-	overallBookingPriceDiv.innerHTML = "<p>" + localStorage.getItem("overallBookingPrice") + "</p>";
-	if (overallBookingPriceDiv.innerHTML == "<p>null</p>") {
-		overallBookingPriceDiv.innerHTML = "<p>0</p>";
-	}
+bookBtn.addEventListener("click", async function() {
+   calculateBooking()
+  bookingDetailsDiv.innerHTML =
+    "<p>Room Type: " +
+    roomType.value +
+    "</p><p>Number of Rooms: " +
+    numberOfRooms.value +
+    "</p><p>Check In Date: " +
+    checkIn.value +
+    "</p><p>Check Out Date: " +
+    checkOut.value +
+    "</p><p>Number of Adults: " +
+    numberOfAdults.value +
+    "</p><p>Number of Children: " +
+    numberOfChildren.value +
+    "</p><p>Time Duration: " +
+    timeDuration.value +
+    " Days</p><p>Extra Requirements:" +
+    extraRequirements +
+    "</p><br>";
+  currentBookingPriceDiv.innerHTML = "<p> " + currentBookingPrice + "</p>";
+  overallBookingDiv.innerHTML = localStorage.getItem("overallBookingDiv");
+  overallBookingPriceDiv.innerHTML = "<p> " + overallBookingPrice + "</p>";
+    // "<p>" + localStorage.getItem("overallBookingPrice") + "</p>";
+  if (overallBookingPriceDiv.innerHTML == "<p>null</p>") {
+    overallBookingPriceDiv.innerHTML = "<p>0</p>";
+  }
 });
 
 resetCurrentBookingBtn.addEventListener("click", function () {
-	window.location.reload();
-	extraRequirements = [];
+  window.location.reload();
+  extraRequirements = [];
 });
 
 checkLoyaltyBtn.addEventListener("click", function () {
-	if (numberOfRooms.value >= 3) {
-		loyaltyPoints = 20*numberOfRooms.value;
-	} else if (numberOfRooms.value < 3) {
-		loyaltyPoints = 'No Loyalty Points';
-	}
-	loyaltyPointsDiv.innerHTML = loyaltyPoints;
+  if (numberOfRooms.value >= 3) {
+    loyaltyPoints = 20 * numberOfRooms.value;
+  } else if (numberOfRooms.value < 3) {
+    loyaltyPoints = "No Loyalty Points";
+  }
+  loyaltyPointsDiv.innerHTML = loyaltyPoints;
 });
 
-
 addToFavBtn.addEventListener("click", function () {
-	overallBookingDiv.innerHTML += bookingDetailsDiv.innerHTML;
-	localStorage.setItem("overallBookingDiv", overallBookingDiv.innerHTML);
-	localStorage.setItem("overallBookingPrice", overallBookingPrice += currentBookingPrice);
+	
+  overallBookingDiv.innerHTML += bookingDetailsDiv.innerHTML;
+  localStorage.setItem("overallBookingDiv", overallBookingDiv.innerHTML);
+  localStorage.setItem(
+    "overallBookingPrice",
+    (overallBookingPrice += currentBookingPrice)
+  );
+  overallBookingPriceDiv.innerHTML = "<p> " + overallBookingPrice + "</p>";
 });
