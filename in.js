e3cd3fe2e781c721efdf2cf9cabbae5604966266
pramishka22 <143793055+pramishka22@users.noreply.github.com>
2024-01-12 09:12:33 +0530

    Update in.js

--- in.js
+++ in.js
@@ -89,7 +89,7 @@ resetCurrentBookingBtn.addEventListener("click", function () {
 });
 
 checkLoyaltyBtn.addEventListener("click", function () {
-	if (numberOfRooms.value >= 3) {
+	if (numberOfRooms.value >= 4) {
 		loyaltyPoints = 20*numberOfRooms.value;
 	} else if (numberOfRooms.value < 3) {
 		loyaltyPoints = 'No Loyalty Points';
